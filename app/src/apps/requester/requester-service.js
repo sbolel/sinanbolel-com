5e10c87f33e45b3345a118c8682f6c19458f6004
Sinan Bolel <dev@sinanbolel.com>
2015-07-16 04:39:57 -0400

    Requester app completes a get request.

--- app/src/apps/requester/requester-service.js
+++ app/src/apps/requester/requester-service.js
@@ -1,9 +1,86 @@
 var requesterModule = angular.module('thinkcrazy.apps.requester.service', [])
 
-.service('RequesterService', ['$log', function($log){
+.service('RequesterService', ['$log', '$q', '$http', function($log, $q, $http){
 
 var that = this;
+var requestsArray = [];
+var responseArray = [];
 
-return that;
+var Request = function(url, params){
+  if(typeof(url)!=='undefined'){
+    this.url = url;
+  } else {
+    this.url = '';
+  }
+  if(typeof(params)!=='undefined'){
+    this.params = params;
+  } else {
+    this.params = {};
+  }
+  this.response = {
+    data: {},
+    status: {},
+    headers: {},
+    config: {}
+  };
+  this.key = requestsArray.length;
+  requestsArray.push(this);
+  return this;
+};
+
+Request.prototype.setResponse = function(data, status, headers, config){
+  this.response = {
+    data: data,
+    status: status,
+    headers: headers,
+    config: config
+  };
+};
+
+Request.prototype.execute = function(){
+  var that = this;
+  var deferred = $q.defer();
+  if(typeof(this.url)==='undefined'||typeof(this.params)==='undefined'){
+    deferred.reject('Missing request url/params.');
+  }
+  $http.get(this.url, this.params).
+    success(function(data, status, headers, config) {
+      that.setResponse(data, status, headers, config);
+      deferred.resolve(data, status, headers, config);
+    }).
+    error(function(data, status, headers, config) {
+      that.setResponse(data, status, headers, config);
+      deferred.reject(data, status, headers, config);
+    });
+  return deferred.promise;
+};
+
+Request.prototype.cache = function(){
+  
+};
+
+this.getRequestByKey = function(requestKey){
+  if(typeof(requestsArray[requestKey])!=='undefined'){
+    return requestsArray[requestKey];
+  } else {
+    return null;
+  }
+};
+
+this.createRequest = function(url, params){
+  var deferred = $q.defer()
+  var req = new Request(url, params);
+  req.execute().then(function(data, status, headers, config){
+    $log.debug('Completed request:\n', data, status, headers, config);
+    deferred.resolve(data, status, headers, config);
+  }).catch(function(data, status, headers, config){
+    $log.error('Error completing request\n:', data, status, headers, config);
+    deferred.reject(data, status, headers, config);
+  });
+  return deferred.promise;
+};
+
+
+return this;
 
 }]);
--- app/src/apps/requester/requester.js
+++ app/src/apps/requester/requester.js
@@ -11,8 +11,28 @@ requesterModule.config(function ($stateProvider) {
     });
 });
 
-requesterModule.controller('RequesterAppController', ['$log', function($log){
+requesterModule.controller('RequesterAppController', ['$log', '$scope', 'RequesterService', function($log, $scope, RequesterService){
 
+  $scope.requester = {
+    $loading: false,
+    $requestKeys: [],
+    params: {}
+  };
 
+  $scope.makeRequest = function(){
+    $scope.requester.$loading = true;
+    RequesterService.createRequest($scope.requester.url, $scope.requester.params)
+      .then(function(data, status, headers, config){
+        $scope.requester.response = data;
+    }).catch(function(key, data, status, headers, config){
+        $scope.requester.response = data;
+    });
+  };
+
+  function detectResponseType(){
+    var key = $scope.requester.$requestKeys[$scope.requester.$requestKeys.length=1];
+    var res = RequesterService.getRequestByKey(key);
+    $log.debug('Got request[',key,']: ',res);
+  };
 
 }]);
