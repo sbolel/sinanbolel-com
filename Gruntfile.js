4c45f60c4fbb3bf9ea854b049fb9746c54f93529
Sinan Bolel <sbolel@users.noreply.github.com>
2020-09-17 14:05:17 -0400

    feat: add new styles, remove insta

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,98 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-module.exports = function(grunt) {
+const pkg = require('./package.json')
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+const clean = {
+  release: ['public/release/*.js']
+}
 
-    clean: {
-      release: ['public/release/*.js']
+const connect = {
+  client: {
+    options: {
+      base: './public',
+      livereload: true,
+      open: {
+        appName: 'Google Chrome',
+        target: 'http://localhost:9000',
+      },
+      port: 9000,
     },
+  },
+}
 
-    connect: {
-      client: {
-        options: {
-          port: 9000,
-          base: './public',
-          livereload: true,
-          open: {
-            target: 'http://localhost:9000',
-            appName: 'Chrome',
-          }
-        }
-      }
+const cssmin = {
+  options: {
+    keepSpecialComments: false,
+    shorthandCompacting: true,
+    sourceMap: false,
+  },
+  target: {
+    files: {
+      'public/<%= pkg.name %>.min.css': [
+        'public/assets/css/*.css',
+      ]
     },
+  },
+}
 
-    cssmin: {
+const htmlmin = {
+  dist: {
+    options: {
+      removeComments: true,
+      collapseWhitespace: true,
+    },
+    files: { 'public/index.html': './index.html', }
+  }
+}
+
+const watch = {
+  css: {
+    files: ['public/assets/css/*.css'],
+    tasks:['cssmin', 'htmlmin', 'replace'],
+  },
+  html: {
+    files: ['public/**/*.html'],
+    options: {
+      livereload: true,
+    },
+  },
+}
+
+const plugins = [
+  'grunt-contrib-clean',
+  'grunt-contrib-connect',
+  'grunt-contrib-cssmin',
+  'grunt-contrib-htmlmin',
+  'grunt-contrib-watch',
+  'grunt-replace',
+]
+
+module.exports = ({
+  initConfig,
+  loadNpmTasks,
+  option,
+  registerTask,
+ }) => {
+  const replace = {
+    dist: {
       options: {
-        sourceMap: false,
-        shorthandCompacting: true,
-        keepSpecialComments: false
+        patterns: [{
+          match: 'CSS_FILE_HASH',
+          replacement: () => `${option('target')}`
+        }]
       },
-      target: {
-        files: {
-          'public/<%= pkg.name %>.min.css': [
-            'public/assets/css/*.css'
-          ]
-        }
-      }
-    },
-
-    watch: {
-      html: {
-        files: ['public/**/*.html'],
-        options: {
-          livereload: true
-        }
-      },
-      css: {
-        files: ['public/assets/css/*.css'],
-        tasks:['cssmin']
-      }
+      files: [
+        { dest: 'public/', expand: true, flatten: true, src: ['public/index.html'] }
+      ]
     }
+  }
 
-  });
+  initConfig({ clean, connect, cssmin, htmlmin, pkg, replace, watch })
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  plugins.forEach(loadNpmTasks)
 
-  grunt.registerTask('serve', ['connect', 'watch']);
-  grunt.registerTask('default', ['cssmin', 'serve']);
-};
+  registerTask('serve', ['connect', 'watch'])
+  registerTask('build', ['cssmin', 'htmlmin'])
+  registerTask('default', ['build', 'serve'])
+}
