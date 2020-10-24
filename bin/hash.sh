0ee1547059c6a8aafbfc8efbd91c1340990efa32
Sinan Bolel <sbolel@users.noreply.github.com>
2020-10-24 14:13:31 -0400

    fix: correct generation of css target

--- bin/hash.sh
+++ bin/hash.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "public/styles-$(node bin/hash.js ./public/sinanbolel.min.css | grep 'hash:' | cut -c6-13).min.css"
+echo "styles-$(node bin/hash.js ./public/sinanbolel.min.css | grep 'hash:' | cut -c6-13).min.css"
